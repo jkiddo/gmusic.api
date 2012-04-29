2c76798bcbdfa0dbaa5589291fc0937551a35f7e
Jens Krististian Villadsen <jenskristianvilladsen@gmail.com>
2012-04-29 16:28:51 +0200

    * Updated example

--- src/test/java/test/APIexample.java
+++ src/test/java/test/APIexample.java
@@ -11,25 +11,31 @@
 package test;
 
 import gmusic.api.impl.GoogleMusicAPI;
+import gmusic.api.impl.GoogleSkyJamAPI;
 import gmusic.api.interfaces.IGoogleMusicAPI;
 import gmusic.api.model.Playlist;
 import gmusic.api.model.Playlists;
 import gmusic.api.model.Song;
 
-import java.io.File;
-import java.net.URL;
-import java.util.Collection;
-
-import org.apache.commons.io.FileUtils;
-
 public class APIexample
 {
 	public static void main(String args[])
 	{
+		GoogleSkyJamAPI skyJam = new GoogleSkyJamAPI();
+
+		try
+		{
+			skyJam.login("1234@gmail.com", "1234");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
 		IGoogleMusicAPI api = new GoogleMusicAPI();
 		try
 		{
-			api.login("someemail@gmail.com", "myPass");
+			api.login("1234@gmail.com", "1234");
 			Playlists playlists = api.getAllPlaylists();
 			for(Playlist list : playlists.getMagicPlaylists())
 			{
@@ -48,13 +54,8 @@ public class APIexample
 					System.out.println(song.getName() + " " + song.getArtist());
 				}
 			}
-			Collection<Song> songs = api.getAllSongs();
-			for(Song song : songs)
-			{
-				System.out.println(song.getName());
-				System.out.println(api.getSongURL(song.getId()).getUrl());
-//				FileUtils.copyURLToFile(new URL(api.getSongURL(song.getId()).getUrl()), new File(song.getName()));
-			}
+			// Collection<Song> songs = api.getAllSongs();
+			// api.downloadSongs(songs);
 		}
 		catch(Exception e)
 		{
