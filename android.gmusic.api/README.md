7d73cc5f36f79a2e0b7026bbae7066b59af4498c
Baron Hall <bhall@mezeo.com>
2013-03-13 11:31:30 -0500

    Http client tweak
    This mod works around an issue with the loopj project. Two fields in the
    base class are private when they should be protected, So we add a copy
    of the field definitions here.

--- android.gmusic.api/README.md
+++ android.gmusic.api/README.md
@@ -2,19 +2,6 @@
 #android package
 
 As currently built, the android package is hardwired to the loopj/android-async-http project, which is also on github. Be sure to pull that down and reference it as an Android library project reference.
-There are two changes that need to be made to the loopj project. An issue has been submitted to the loopj project authors, but as yet, it has not been resolved.
-
-Find the SyncHttpClient class in the loopj project. Within that class, find these two lines (lines 17 and 18 at the time of this writing).
-
-private String result;
-
-AsyncHttpResponseHandler responseHandler = new AsyncHttpResponseHandler() {...
-
-*And change them to:*
-
-*protected* String result;
-
-*protected* AsyncHttpResponseHandler responseHandler = new AsyncHttpResponseHandler() {...
 
 This project is licensed under GNU GPL, Version 3.
 
--- android.gmusic.api/src/com/android/gm/api/comm/GmHttpClient.java
+++ android.gmusic.api/src/com/android/gm/api/comm/GmHttpClient.java
@@ -3,10 +3,46 @@ package com.android.gm.api.comm;
 import org.apache.http.HttpEntity;
 
 import android.content.Context;
+import android.os.Message;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.SyncHttpClient;
 
 public class GmHttpClient extends SyncHttpClient {
+	private int responseCode;
+
+	/*
+	 * as this is a synchronous request this is just a helping mechanism to pass
+	 * the result back to this method. Therefore the result object has to be a
+	 * field to be accessible
+	 */
+	private String result;
+	private AsyncHttpResponseHandler responseHandler = new AsyncHttpResponseHandler() {
+
+		protected void sendResponseMessage(org.apache.http.HttpResponse response) {
+			responseCode = response.getStatusLine().getStatusCode();
+			super.sendResponseMessage(response);
+		};
+
+		@Override
+		protected void sendMessage(Message msg) {
+			/*
+			 * Dont use the handler and send it directly to the analysis
+			 * (because its all the same thread)
+			 */
+			handleMessage(msg);
+		}
+
+		@Override
+		public void onSuccess(String content) {
+			result = content;
+		}
+
+		@Override
+		public void onFailure(Throwable error, String content) {
+			result = onRequestFailed(error, content);
+		}
+	};
 
 	public GmHttpClient() {
 		super();
@@ -21,5 +57,14 @@ public class GmHttpClient extends SyncHttpClient {
 	public String onRequestFailed(Throwable error, String content) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	/**
+	 * @return the response code for the last request, might be usefull
+	 *         sometimes
+	 */
+	@Override
+	public int getResponseCode() {
+		return responseCode;
 	}
 }
